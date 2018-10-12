2e96946e293ace67b432a27c5c40039671928500
reedk10 <40337728+reedk10@users.noreply.github.com>
2018-10-11 21:42:38 -0400

    Add files via upload

---
+++ todo Lab/components/todo/todo-controller.js
@@ -0,0 +1,41 @@
+"use strict";
+{
+
+    angular.module('app')
+      .controller('TodoController', function(){
+
+        const $ctrl= this;
+
+        $ctrl.list=[
+            {todo: "Workout", complete: true},
+            {todo: "Wash the dog", complete: true},
+            {todo: "Walk the dog", complete: false},
+            {todo: "Go grocery shopping", complete: true},
+            {todo: "Do homework", complete: false}
+        ];
+
+        $ctrl.add = function(){
+
+            $ctrl.list.push({todo:$ctrl.toDos, complete:true});
+            console.log($ctrl.list);
+            
+        };
+
+       // $ctrl.toDoTasks.push{list.$ctrl.addTodo, done: false});
+
+        $ctrl.removeTask = function($index) {
+         $ctrl.list.splice($index, 1);
+        };
+
+         $ctrl.completeTask = (index) => {
+            $ctrl.toDoTasks[index].done = true;
+         };
+           
+        
+
+
+
+
+ console.log("this works")
+      });
+}
---
+++ todo Lab/components/todo/todo.js
@@ -0,0 +1,8 @@
+"use strict";
+{
+    angular.module("app")
+    .component("todo", {
+        controller: "TodoController",
+        templateUrl: "components/todo/todo.html"
+    })
+}
